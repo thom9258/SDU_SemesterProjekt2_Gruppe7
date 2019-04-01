e4d6036466f49a0fed887fe8eb7c575c774148cf
knudsen97 <48911177+knudsen97@users.noreply.github.com>
2019-04-01 11:17:14 +0200

    Create FuckCHNI.asm

---
+++ FuckCHNI.asm
@@ -0,0 +1,34 @@
+;
+; 2semProjekt.asm
+;
+; Created: 18-Mar-19 09:39:18
+; Author : Knuds
+;
+
+
+;MAIN LOOP
+MAIN:
+;få indput fra Pin a
+LDI		R16, 0xFF
+OUT		DDRA, R16
+IN		R16, PINA
+
+;compare PA1 ved at rotere den ud til carry
+ror		R16
+ror		R16
+brcs	LED		;branch if carry = 1
+
+;hvis ikke der er input fra PA1, sluk led
+ldi		R16, 0
+out		ddra, R16
+out		porta, R16
+
+RJMP	MAIN
+
+LED:
+LDI		R16, 0x00
+OUT		DDRA, R16
+LDI		R16, 0b01000000
+OUT		PORTA, R16
+
+RJMP	MAIN
